df02278cc5909342ebaea4d2ff11000b974fe6a5
Orion Moonclaw <orion@moonclaw.eu>
2026-05-01 20:08:12 +0200

    video: fix colorspace mismatch

--- crates/renderide/src/assets/video/cpu_copy.rs
+++ crates/renderide/src/assets/video/cpu_copy.rs
@@ -59,7 +59,7 @@ impl SinkState {
             mip_level_count: 1,
             sample_count: 1,
             dimension: wgpu::TextureDimension::D2,
-            format: wgpu::TextureFormat::Rgba8Unorm,
+            format: wgpu::TextureFormat::Rgba8UnormSrgb,
             usage: wgpu::TextureUsages::COPY_DST
                 | wgpu::TextureUsages::TEXTURE_BINDING
                 | wgpu::TextureUsages::COPY_SRC,
