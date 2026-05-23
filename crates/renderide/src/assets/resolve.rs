28d430d1db459630a45edcd15a946948a8f80759
Baplar <me@baplar.fr>
2026-05-23 17:26:50 +0200

    Allow Linux to look for assets in /usr/share
    Also change asset loading order to favor source

--- crates/renderide/src/assets/resolve.rs
+++ crates/renderide/src/assets/resolve.rs
@@ -11,15 +11,6 @@ pub fn assets_search_candidates() -> Vec<PathBuf> {
         }
     };
 
-    if let Ok(exe) = std::env::current_exe()
-        && let Some(dir) = exe.parent()
-    {
-        push_unique(&mut out, dir.join(ASSETS_FOLDER_NAME));
-        if let Some(parent) = dir.parent() {
-            push_unique(&mut out, parent.join(ASSETS_FOLDER_NAME));
-        }
-    }
-
     if let Ok(cwd) = std::env::current_dir() {
         if let Some(root) = crate::config::find_renderide_workspace_root(&cwd) {
             push_unique(
@@ -32,12 +23,22 @@ pub fn assets_search_candidates() -> Vec<PathBuf> {
 
     if let Ok(exe) = std::env::current_exe()
         && let Some(dir) = exe.parent()
-        && let Some(root) = crate::config::find_renderide_workspace_root(dir)
     {
-        push_unique(
-            &mut out,
-            root.join("crates/renderide").join(ASSETS_FOLDER_NAME),
-        );
+        if let Some(root) = crate::config::find_renderide_workspace_root(dir) {
+            push_unique(
+                &mut out,
+                root.join("crates/renderide").join(ASSETS_FOLDER_NAME),
+            );
+        }
+        push_unique(&mut out, dir.join(ASSETS_FOLDER_NAME));
+        if let Some(parent) = dir.parent() {
+            push_unique(&mut out, parent.join(ASSETS_FOLDER_NAME));
+        }
+    }
+
+    #[cfg(target_family = "unix")]
+    {
+        push_unique(&mut out, PathBuf::from("/usr/share/renderide"));
     }
 
     out
